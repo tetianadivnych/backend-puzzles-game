8736fbd84272ace4485f8d1924dc52854ba5ebb0
tetianadivnych <113429084+tetianadivnych@users.noreply.github.com>
2023-06-16 17:07:26 +0300

    init project

---
+++ src/main/java/com/divnych/puzzlesgame/PuzzlesGameApplication.java
@@ -0,0 +1,13 @@
+package com.divnych.puzzlesgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzlesGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzlesGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divnych/puzzlesgame/PuzzlesGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divnych.puzzlesgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzlesGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
