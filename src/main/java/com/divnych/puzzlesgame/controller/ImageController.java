4890afdb888d2b9884150e43a382a6065332f32b
tetianadivnych <113429084+tetianadivnych@users.noreply.github.com>
2023-06-18 19:44:17 +0300

    provide access for frontend

--- src/main/java/com/divnych/puzzlesgame/controller/ImageController.java
+++ src/main/java/com/divnych/puzzlesgame/controller/ImageController.java
@@ -3,10 +3,7 @@ package com.divnych.puzzlesgame.controller;
 import com.divnych.puzzlesgame.converter.ImageConverter;
 import com.divnych.puzzlesgame.playload.ImageUrlRequest;
 import com.divnych.puzzlesgame.service.ImageService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,6 +11,7 @@ import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/images")
 public class ImageController {
